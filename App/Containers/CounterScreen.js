bcf1eeb194534ee607b4414d938b4dc72c424375
Daniel Chong <danielgq.chong@gmail.com>
2016-09-01 06:30:00 +1200

    able to show the board like a board

--- App/Containers/CounterScreen.js
+++ App/Containers/CounterScreen.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native'
 import Button from 'react-native-button'
 import {observer} from 'mobx-react/native'
+import _ from 'lodash'
 
 import CounterRow from '../Components/CounterRow.js'
 import styles from './Styles/CounterScreen.Style.js'
@@ -15,17 +16,19 @@ class Counter extends React.Component {
     let counterArray = this.props.store.counterArray
     let counterO = this.props.store.counter
     let board = this.props.store.board
-    let boardView = board.cells.map((cell) => {
+    let rowView = board.cells.map((cell) => {
       return <Button onPress={cell.bump}>{cell.status}</Button>
     })
+    let boardView = _.chunk(rowView, 9).map((row, i) => {
+        return <View key={i} style={styles.row}>{row}</View>
+    })
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Cell
         </Text>
-        <View style={styles.row}>
-          {boardView}
-        </View>
+        {boardView}
 
         <Text style={styles.welcome}>
           Single object example
